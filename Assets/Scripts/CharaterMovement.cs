618aec7fc579c916aed1f1599cc2afd27c112cfa
CactusFamilyTree <141030665+StoneColdBlue@users.noreply.github.com>
2024-04-02 04:46:21 +0200

    First Up Load
    This is the bare minimum, complete with pick ups, random spawning obstacles and character movement.

---
+++ Assets/Scripts/CharaterMovement.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterMovement : MonoBehaviour
+{
+    bool alive = true; // sets the charater to be alive by default 
+
+    public float pace = 10; //the constant speed of the charater
+    public Rigidbody rb; // tis is to link to the charater
+
+    float horizontalInput; //this is a variable to store the horizontal in puts
+    public float horizontalMulti = 2; // this increses the speed
+
+    float verticalInput; // a variable to store the vertical input
+    public float verticalMulti = 2;//this was a multiplyer but it couses the charater to jump to high
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;//checks if the charater is dead or alive
+
+        Vector3 forwardMove = transform.forward * pace * Time.fixedDeltaTime; //this makes the player move forward continuesly
+        Vector3 horizontalMove = transform.right * horizontalInput * pace * Time.fixedDeltaTime * horizontalMulti;//recives horizontal inputs
+        Vector3 verticalMove = transform.up * verticalInput * pace * Time.fixedDeltaTime * verticalMulti;//recives vertical inputs
+        rb.MovePosition(rb.position + forwardMove + horizontalMove + verticalMove);//compiles all inputs to the charater
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");//gets the input from the player
+        verticalInput = Input.GetAxis("Vertical");//gets the input from the player
+
+        // kills the player if they fall of the map
+        if (transform.position.y < -5)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        //kills the player
+        alive = false;
+        //restarts the game
+        Invoke("DeathScreen", 2);
+    }
+
+    void DeathScreen()//a function to restarts the game 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
+}
---
+++ Assets/Scripts/GroundTrigger.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class GroundTrigger : MonoBehaviour
+{
+    EndlessSpwan groundSpwan;
+    void Start()
+    {
+        groundSpwan = GameObject.FindObjectOfType<EndlessSpwan>();//triggers the tiles to spwan endlessly
+        SpawnObstacles();
+        SpawnPower();
+    }
+
+    private void OnTriggerExit(Collider other)//This is a restart to the game 
+    {
+        groundSpwan.NewTile();//spawns a new game
+        Destroy(gameObject, 2);//Delays the restart by 2 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject obstaclePrefab;
+
+    void SpawnObstacles ()
+    {
+        //random point for spawning obstacle
+        int obstecleSpawnIndex = Random.Range(2, 5);//randomizes between children
+        Transform spawnPoint = transform.GetChild(obstecleSpawnIndex).transform;//this gets the childs transform from unity
+
+        //spwan obstacle
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity/*keeps it from rotating*/, transform );
+    }
+
+    public GameObject powerUpPrefab;//variable to store power ups
+
+    //spawn tokens for power ups but for now is a score token
+    void SpawnPower()
+    {
+        int tokenSpawn = 3;//token amount
+        for (int i = 0; i < tokenSpawn; i++)
+        {
+            GameObject tokenCoin = Instantiate(powerUpPrefab, transform);//spawns token
+            tokenCoin.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+   
+    Vector3 GetRandomPointInCollider (Collider collider)//function to get the location for coin spawn
+    {
+        Vector3 location = new Vector3
+            (
+                Random.Range(collider.bounds.min.x, collider.bounds.max.x),//This sets the random spwan to always be on the platform
+                Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+                Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+
+        if ( location != collider.ClosestPoint(location))
+        {
+            location = GetRandomPointInCollider(collider);//sets the coins to spwan on the platform
+        }
+
+        location.y = 1;//makes the coins spawn on the ground
+        return location;
+    }
+}
